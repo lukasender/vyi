4e77629192c15feecfcda21a4bc02d6e27807c8d
Lukas Ender <hello@lukasender.at>
2014-05-23 00:07:49 +0200

    test data: increased start balance to 100000

--- etc/crate_testdata.sh
+++ etc/crate_testdata.sh
@@ -1,10 +1,10 @@
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"elon_musk","balance":10000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"elon_musk","balance":100000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"albert_einstein","balance":10000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"albert_einstein","balance":100000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"nikola_tesla","balance":10000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"nikola_tesla","balance":100000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"lovelace","balance":10000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"lovelace","balance":100000.00}'
 echo ""
 curl -XPOST -H "Content-Type: application/json" http://localhost:9100/projects/add -d '{"name":"awesome project", "initiator": "elon_musk"}'
 echo ""
