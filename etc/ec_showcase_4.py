433125ed199b73677d60b0ed791b85e51202b575
Lukas Ender <hello@lukasender.at>
2014-05-23 00:08:35 +0200

    parameterised ec_showcase_4

--- etc/ec_showcase_4.py
+++ etc/ec_showcase_4.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import json
 import time
+import argparse
 
 from threading import Thread, Lock
 from Queue import Queue
@@ -10,19 +11,13 @@ from Queue import Queue
 
 USERS = ["elon_musk", "albert_einstein", "nikola_tesla", "lovelace"]
 
-x_transactions = 100
-
-max_concurrent_transactions = 1
-
-transactions_queue = Queue(max_concurrent_transactions * 2)
-
 lock = Lock()
 
 BASEURL = 'http://localhost:9100'
 headers = {'content-type':'application/json'}
 
 
-def release_the_kraken():
+def release_the_kraken(immediate, x_transactions, max_concurrent_transactions):
     try:
         url_users = BASEURL + '/users'
         r = requests.get(url_users)
@@ -31,19 +26,22 @@ def release_the_kraken():
         sender = users[0]
         receiver = users[1]
 
-        args_transaction = (sender, receiver, transactions_queue)
+        transactions_queue = Queue(max_concurrent_transactions * 2)
+
+        args_transaction = (sender, receiver, immediate, transactions_queue)
         start_daemons(max_concurrent_transactions, transaction,
                       args_transaction)
         start_task_and_wait(x_transactions, transactions_queue)
 
-        print "Done!"
+        excepted_balance = (pow(x_transactions, 2) + x_transactions) / 2
+        print "Done! Total balance transfered: {0}".format(excepted_balance)
     except KeyboardInterrupt:
         sys.exit(1)
 
 
-def start_daemons(max_concurrency, target, args):
+def start_daemons(max_concurrency, target, thread_args):
     for _ in range(max_concurrency):
-        th = Thread(target=target, args=args)
+        th = Thread(target=target, args=thread_args)
         th.setDaemon(True)
         th.start()
 
@@ -54,7 +52,7 @@ def start_task_and_wait(num_tasks, task_queue):
     task_queue.join()
 
 
-def transaction(sender, receiver, queue):
+def transaction(sender, receiver, immediate, queue):
     while True:
         try:
             i = queue.get()
@@ -64,7 +62,8 @@ def transaction(sender, receiver, queue):
                 sender['nickname'],
                 receiver['nickname']
             )
-            url = BASEURL + '/transactions/u2u'
+            path = 'u2u_immediate' if immediate else 'u2u'
+            url = BASEURL + '/transactions/' + path
             payload = {
                 'sender': sender['id'],
                 'receiver': receiver['id'],
@@ -112,4 +111,30 @@ class Timer(object):
 
 
 if __name__ == '__main__':
-    release_the_kraken()
+
+    parser = argparse.ArgumentParser(
+        description='Transfer user balance concurrently.'
+    )
+    parser.add_argument(
+        '-i',
+        '--immediate',
+        action='store_true',
+        required=False,
+        help='process the transactions immediately'
+    )
+    parser.add_argument(
+        '-t',
+        '--transactions',
+        type=int,
+        default=100,
+        required=False
+    )
+    parser.add_argument(
+        '-c',
+        '--concurrent',
+        type=int,
+        default=10,
+        required=False
+    )
+    args = parser.parse_args()
+    release_the_kraken(args.immediate, args.transactions, args.concurrent)
